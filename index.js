2b437923673695ebf76690ff37354013997c62b7
Alexandre Junior <alexandrejunior408@gmail.com>
2022-07-26 17:01:05 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+// config inicial
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+
+// models
+const Person = require('./models/Person');
+
+// forma de ler JSON / middlewares
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// rotas da API
+const personRoutes = require('./routes/personRoutes');
+
+app.use('/person', personRoutes); // tudo que vier de /routes irá ser redirecionado para personRoutes
+
+// rota inicial / endpoint
+app.get('/', (req, res) => {
+  // mostrar requisição
+  res.json({ message: 'OI Express!' });
+});
+
+// entregar uma porta
+const DB_USER = 'admin';
+const DB_PASSWORD = encodeURIComponent('admin');
+
+mongoose
+  .connect(
+    `mongodb+srv://${DB_USER}:${DB_PASSWORD}@apicluster.kw6uz.mongodb.net/bancodaapi?retryWrites=true&w=majority`,
+  )
+  .then(() => {
+    console.log('Conectamos ao MongoDB!');
+    app.listen(3000);
+  })
+  .catch((error) => console.log(error));
